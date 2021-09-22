b6e4c6a2bce10cceba5c6756150dbf0582bb6f63
Alesana Lealofi Eteuati Jr <junior.eteuati@gmail.com>
2021-09-23 01:13:03 +1300

    Display description with syntax highlighting

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { API, graphqlOperation } from "aws-amplify";
 import { createTodo } from "../graphql/mutations";
 import { listTodos } from "../graphql/queries";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 const initialState = { name: "", description: "" };
 
@@ -39,57 +41,59 @@ export default function Todo() {
     }
   }
   return (
-      <div style={styles.container}>
-        <h2>Owl Todos</h2>
-        <input
-          onChange={(event) => setInput("name", event.target.value)}
-          style={styles.input}
-          value={formState.name}
-          placeholder="Name"
-        />
-        <input
-          onChange={(event) => setInput("description", event.target.value)}
-          style={styles.input}
-          value={formState.description}
-          placeholder="Description"
-        />
-        <button style={styles.button} onClick={addTodo}>
-          Make Magic
-        </button>
-        {todos.map((todo, index) => (
-          <div key={todo.id ? todo.id : index} style={styles.todo}>
-            <p style={styles.todoName}>{todo.name}</p>
-            <p style={styles.todoDescription}>{todo.description}</p>
-          </div>
-        ))}
-      </div>
+    <div style={styles.container}>
+      <h2>Owl Todos</h2>
+      <input
+        onChange={(event) => setInput("name", event.target.value)}
+        style={styles.input}
+        value={formState.name}
+        placeholder="Name"
+      />
+      <input
+        onChange={(event) => setInput("description", event.target.value)}
+        style={styles.input}
+        value={formState.description}
+        placeholder="Description"
+      />
+      <button style={styles.button} onClick={addTodo}>
+        Make Magic
+      </button>
+      {todos.map((todo, index) => (
+        <div key={todo.id ? todo.id : index} style={styles.todo}>
+          <p style={styles.todoName}>{todo.name}</p>
+          <SyntaxHighlighter language="javascript" style={docco}>
+            {todo.description}
+          </SyntaxHighlighter>
+        </div>
+      ))}
+    </div>
   );
 }
 
 const styles = {
-    container: {
-      width: 400,
-      margin: "0 auto",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      padding: 20,
-    },
-    todo: { marginBottom: 15 },
-    input: {
-      border: "none",
-      backgroundColor: "#ddd",
-      marginBottom: 10,
-      padding: 8,
-      fontSize: 18,
-    },
-    todoName: { fontSize: 20, fontWeight: "bold" },
-    todoDescription: { marginBottom: 0 },
-    button: {
-      backgroundColor: "black",
-      color: "white",
-      outline: "none",
-      fontSize: 18,
-      padding: "12px 0px",
-    },
-  };
+  container: {
+    width: 400,
+    margin: "0 auto",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    padding: 20,
+  },
+  todo: { marginBottom: 15 },
+  input: {
+    border: "none",
+    backgroundColor: "#ddd",
+    marginBottom: 10,
+    padding: 8,
+    fontSize: 18,
+  },
+  todoName: { fontSize: 20, fontWeight: "bold" },
+  todoDescription: { marginBottom: 0 },
+  button: {
+    backgroundColor: "black",
+    color: "white",
+    outline: "none",
+    fontSize: 18,
+    padding: "12px 0px",
+  },
+};
