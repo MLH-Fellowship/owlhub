f09c35f153205a95b48f102188e829b28d313e2b
Alesana Lealofi Eteuati Jr <junior.eteuati@gmail.com>
2021-09-23 01:36:08 +1300

    Change font

--- src/App.js
+++ src/App.js
@@ -5,13 +5,23 @@ import Amplify, {  } from "aws-amplify";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import awsExports from "./aws-exports";
 import Todo from "./components/Todo";
+import { extendTheme } from "@chakra-ui/react"
+import "@fontsource/league-mono/400.css"
+
+
+const theme = extendTheme({
+  fonts: {
+    heading: "League Mono",
+    body: "League Mono",
+  },
+})
 
 Amplify.configure(awsExports);
 
 const App = () => {
   
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Todo></Todo>
     </ChakraProvider>
   );
