358e2dc95836660f1aac8d86535c73bc5498226b
Alesana Lealofi Eteuati Jr <junior.eteuati@gmail.com>
2021-09-24 07:30:19 +1300

    Remove declarations

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,16 +2,8 @@ import { useEffect, useState } from "react";
 import { API, graphqlOperation } from "aws-amplify";
 import { Container } from "@chakra-ui/react";
 import { listTodos } from "../graphql/queries";
-import SyntaxHighlighter from "react-syntax-highlighter";
-import { dark } from "react-syntax-highlighter/dist/esm/styles/hljs";
-import { VStack } from "@chakra-ui/react";
-import { Input } from "@chakra-ui/react";
-import { useCounter } from "@chakra-ui/counter"
-import {MoonIcon} from "@chakra-ui/icons";
-import { Textarea } from "@chakra-ui/react";
-import { Button, useColorModeValue } from "@chakra-ui/react";
-
-const initialState = { name: "", description: "" };
+import { PostForm } from "./PostForm";
+import { Card } from "./Card";
 
 export default function Todo() {
   const [todos, setTodos] = useState([]);
@@ -34,9 +26,12 @@ export default function Todo() {
     <Container maxW="container.md">
       <PostForm newTodo={(newTodo) => setTodos([...todos, newTodo])}></PostForm>
 
-      {todos.slice(0).reverse().map((todo, index) => (
-        <Card todo={todo} key={index}></Card>
-      ))}
+      {todos
+        .slice(0)
+        .reverse()
+        .map((todo, index) => (
+          <Card todo={todo} key={index}></Card>
+        ))}
     </Container>
   );
 }
